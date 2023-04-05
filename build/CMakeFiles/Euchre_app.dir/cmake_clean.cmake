2ef842a5e92c0ddb52d7092d0d81ce1e4386a850
Dez Boyle <dezjames123@gmail.com>
2023-04-04 21:04:36 -0500

    Initial Commit

---
+++ build/CMakeFiles/Euchre_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Euchre_app.dir/main.cpp.o"
+  "CMakeFiles/Euchre_app.dir/main.cpp.o.d"
+  "Euchre_app.exe"
+  "Euchre_app.pdb"
+  "libEuchre_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Euchre_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Euchre_app.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Euchre_app.
+# This may be replaced when dependencies are built.
